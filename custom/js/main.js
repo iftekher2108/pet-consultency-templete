3800d799409747215738251710f0ad6b4e738137
iftekher2108 <parvezkhan21082002@gmail.com>
2024-03-11 18:13:26 +0600

    mejor ui changed

--- custom/js/main.js
+++ custom/js/main.js
@@ -1,1 +1,14 @@
 console.log("iftekher mahmaud pervez")
+
+
+// copyright date
+$('.date').text(new Date().getFullYear());
+
+// copyright date
+
+// windows loading
+$(document).ready(function(){
+    $('#loader').fadeOut(300)
+})
+// windows loading
+
