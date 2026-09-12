605b21c7d911036be8afe33bc418315fb531ef91
iftekher2108 <parvezkhan21082002@gmail.com>
2026-09-12 22:16:19 +0600

    pet consultency template complate

--- custom/js/main.js
+++ custom/js/main.js
@@ -1,14 +1,48 @@
-console.log("iftekher mahmaud pervez")
+$(function () {
+    $('.date').text(new Date().getFullYear());
 
+    window.setTimeout(function () {
+        $('#loader').css({ opacity: 0, visibility: 'hidden' });
+    }, 350);
 
-// copyright date
-$('.date').text(new Date().getFullYear());
+    if (window.AOS) {
+        AOS.init({ offset: 90, duration: 700, easing: 'ease-out-cubic', once: true });
+    }
 
-// copyright date
+    $('.navbar-nav .nav-link, .navbar-nav .nav-cta').on('click', function () {
+        $('.navbar-collapse').collapse('hide');
+    });
 
-// windows loading
-$(document).ready(function(){
-    $('#loader').fadeOut(300)
-})
-// windows loading
+    $('.filter-btn').on('click', function () {
+        var filter = $(this).data('filter');
+        $('.filter-btn').removeClass('active');
+        $(this).addClass('active');
+        $('.gallery-item').each(function () {
+            var visible = filter === 'all' || $(this).data('category') === filter;
+            $(this).toggle(visible);
+        });
+    });
 
+    $('.gallery-item').on('click', function () {
+        $('#lightbox img').attr('src', $(this).data('image'));
+        $('#lightbox').addClass('open');
+    });
+
+    $('.lightbox, .lightbox-close').on('click', function (event) {
+        if (event.target === this || $(event.target).closest('.lightbox-close').length) {
+            $('#lightbox').removeClass('open');
+        }
+    });
+
+    $(document).on('keydown', function (event) {
+        if (event.key === 'Escape') {
+            $('#lightbox').removeClass('open');
+        }
+    });
+
+    $('#contactForm').on('submit', function (event) {
+        event.preventDefault();
+        $('#formSuccess').addClass('visible');
+        this.reset();
+    });
+});
